35be1547936bf79bd7260d2b4c746a2aa111dddc
Jakub Vrana <jakvra@gmail.com>
2021-02-19 17:13:30 +0100

    custom mysql-service-account.sql based on docker ip

--- src/main/SQL/mysql-service-account.sql
+++ src/main/SQL/mysql-service-account.sql
@@ -1,6 +1,6 @@
-CREATE USER 'springframework'@'localhost' IDENTIFIED BY 'guru';
+CREATE USER 'springframework'@'172.17.0.1' IDENTIFIED BY 'guru';
 
-GRANT SELECT ON springguru.* to 'springframework'@'localhost';
-GRANT INSERT ON springguru.* to 'springframework'@'localhost';
-GRANT DELETE ON springguru.* to 'springframework'@'localhost';
-GRANT UPDATE ON springguru.* to 'springframework'@'localhost';
+GRANT SELECT ON springguru.* to 'springframework'@'172.17.0.1';
+GRANT INSERT ON springguru.* to 'springframework'@'172.17.0.1';
+GRANT DELETE ON springguru.* to 'springframework'@'172.17.0.1';
+GRANT UPDATE ON springguru.* to 'springframework'@'172.17.0.1';
